3b67c8fa69501526a8fba071f02f0c90d7706b56
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-08 17:47:44 +0100

    Altered script variable names

--- dashboard/deploy.sh
+++ dashboard/deploy.sh
@@ -1,5 +1,5 @@
 source .env
-aws ecr get-login-password --region eu-west-2| docker login --username AWS --password-stdin $ECR_URI
+aws ecr get-login-password --region $REGION| docker login --username AWS --password-stdin $ECR_URI
 docker build -t $IMAGE_NAME . --platform "linux/amd64"
 docker tag $IMAGE_NAME:latest $ECR_URI:latest
 docker push $ECR_URI:latest
