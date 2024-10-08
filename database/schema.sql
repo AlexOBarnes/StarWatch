1084f902b34908b6ed397b661440e50dd115a9f8
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-08 11:31:10 +0100

    Added not null and auto generated image id

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS aurora_colour, image;
 
 CREATE TABLE image (
-    image_id BIGINT,
-    image_name VARCHAR(10),
-    image_url VARCHAR(255),
-    image_date DATE,
+    image_id BIGINT GENERATED ALWAYS AS IDENTITY,
+    image_name VARCHAR(10) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    image_date DATE NOT NULL,
     PRIMARY KEY (image_id)
 );
 
 CREATE TABLE aurora_colour (
     aurora_colour_id SMALLINT,
-    colour VARCHAR(6),
-    description VARCHAR(30),
-    meaning VARCHAR(50),
+    colour VARCHAR(6) NOT NULL,
+    description VARCHAR(30) NOT NULL,
+    meaning VARCHAR(50) NOT NULL,
     PRIMARY KEY (aurora_colour_id)
 );
