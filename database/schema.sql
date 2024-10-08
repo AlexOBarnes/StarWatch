52b856dc766987753a29e3a97e18720e65e0165a
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-08 11:48:43 +0100

    Added constellation and body tables sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS region, aurora_alert, aurora_colour, image;
+DROP TABLE IF EXISTS body, constellation, region, aurora_alert, aurora_colour, image;
 
 CREATE TABLE image (
     image_id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -12,7 +12,7 @@ CREATE TABLE aurora_colour (
     aurora_colour_id SMALLINT,
     colour VARCHAR(6) NOT NULL,
     description VARCHAR(30) NOT NULL,
-    meaning VARCHAR(50) NOT NULL,
+    meaning VARCHAR(255) NOT NULL,
     PRIMARY KEY (aurora_colour_id)
 );
 
@@ -30,4 +30,18 @@ CREATE TABLE region (
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
     PRIMARY KEY (region_id)
+);
+
+CREATE TABLE constellation (
+    constellation_id SMALLINT,
+    constellation_name VARCHAR(35) NOT NULL,
+    PRIMARY KEY (constellation_id)
+)
+
+CREATE TABLE body (
+    body_id BIGINT,
+    body_name VARCHAR(35) NOT NULL,
+    constellation_id SMALLINT,
+    PRIMARY KEY (body_id),
+    FOREIGN KEY (constellation_id) REFERENCES constellation(constellation_id)
 );
