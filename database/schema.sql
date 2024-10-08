636f6edf1fdab2de7f676ae760500ff4c3068bfc
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-08 11:35:27 +0100

    Added region table sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS aurora_alert, aurora_colour, image;
+DROP TABLE IF EXISTS region, aurora_alert, aurora_colour, image;
 
 CREATE TABLE image (
     image_id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -22,4 +22,12 @@ CREATE TABLE aurora_alert (
     aurora_colour_id SMALLINT NOT NULL,
     PRIMARY KEY (alert_id),
     FOREIGN KEY (aurora_colour_id) REFERENCES aurora_colour(aurora_colour_id)
+);
+
+CREATE TABLE region (
+    region_id SMALLINT,
+    region_name VARCHAR(25) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    PRIMARY KEY (region_id)
 );
