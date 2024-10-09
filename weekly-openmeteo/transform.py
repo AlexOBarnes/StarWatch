75b2b5a7903e529616b06247d04c8dc2d017aff8
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-09 19:53:51 +0100

    Added transform function

--- weekly-openmeteo/transform.py
+++ weekly-openmeteo/transform.py
@@ -18,7 +18,21 @@ def format_data(clean_county_data: dict, county_id: int) -> list[list]:
 
 def transform(data: list[dict]) -> list[list]:
     '''Returns a list of lists containing county_id, sunset and sunrise times'''
-    ...
+    if not data:
+        raise ValueError('No data to transform.')
+
+    formatted_data = []
+    logging.info('Data transformation begun.')
+
+    for county_id,county in enumerate(data,1):
+        logging.info('Cleaning data for county: %s.',county_id)
+        clean_county = clean_data(county)
+        formatted_data.extend(format_data(clean_county,county_id))
+        logging.info('Data cleaning for county %s complete.', county_id)
+
+    logging.info('Data tranformation complete.')
+    logging.info('Number of rows to insert: %s',len(formatted_data))
+    return formatted_data
 
 
 if __name__ == '__main__':
