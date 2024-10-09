66b544c34f1598a35fe7f6efc5d79d554f15994b
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-09 18:39:17 +0100

    Planned functions

--- weekly-openmeteo/transform.py
+++ weekly-openmeteo/transform.py
@@ -1,1 +1,28 @@
 '''Transforms solar data and ready for loading'''
+import logging
+from dotenv import load_dotenv
+from extract import get_connection
+
+def get_county_ids() -> dict:
+    '''Returns a dictionary of longitude to county_id'''
+    ...
+
+def clean_data(county_data: dict) -> dict:
+    '''Returns a dictionary of county_id, sunset and sunrise times'''
+    ...
+
+def format_data(clean_county_data: dict) -> list[list]:
+    '''Returns a list of lists containing county_id, sunset and sunrise times'''
+    ...
+
+
+def transform(data: list[dict]) -> list[list]:
+    '''Returns a list of lists containing county_id, sunset and sunrise times'''
+    ...
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+    load_dotenv()
+    logging.info('Environment variables loaded.')
+    transform()
