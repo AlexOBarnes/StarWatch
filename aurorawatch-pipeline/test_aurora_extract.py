1d1c55ddb02b4ebc70518c44518663e67d0e6e2f
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-10 11:54:21 +0100

    Started on tests

--- aurorawatch-pipeline/test_aurora_extract.py
+++ aurorawatch-pipeline/test_aurora_extract.py
@@ -2,16 +2,27 @@
 
 from unittest.mock import patch
 import pytest
+from requests import Response
 
-# from aurora_extract import make_request, get_status, extract
+from aurora_extract import make_request, get_status, extract
 
 
 class TestMakeRequest():
     '''Tests for the make request function.'''
 
-    def test_make_request_success_returns_bytes(self):
+    @patch('aurora_extract.requests.get')
+    def test_make_request_success_returns_bytes(self, mock_get, valid_XML):
         '''Tests that the a bytes object is returned when the
         request is successful.'''
+        mock_response = Response()
+        mock_response.status_code = 200
+        mock_response._content = valid_XML
+        mock_get.return_value = mock_response
+
+        data = make_request()
+
+        assert data == valid_XML
+        assert isinstance(data, bytes)
 
     def test_make_request_fail(self):
         '''Tests that an error is raised if the request fails.'''
@@ -24,9 +35,12 @@ class TestGetStatus():
         '''Tests that get status runs correctly and returns a string
         given a valid XML as bytes input.'''
 
-    @mark.par
-    def test_get_status_invalid_input(self):
+    @pytest.mark.parametrize('input', [(1), (['list', 'test']),
+                                       ('test'), ((1, 2)), ({})])
+    def test_get_status_invalid_input(self, input):
         '''Tests that a non-bytes object results in a TypeError.'''
+        with pytest.raises(TypeError):
+            get_status(input)
 
     def test_get_status_no_root(self):
         '''Tests that get status raises a ParseError if no root is found
