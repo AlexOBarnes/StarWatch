710209b5d17bbcce20405ea3ecda0880391afc2b
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-10 18:30:29 +0100

    planned functions

--- quadhoral-openmeteo/quadhoral_load.py
+++ quadhoral-openmeteo/quadhoral_load.py
@@ -1,1 +1,16 @@
 '''Loads cleaned data into the RDS instance'''
+import logging
+from dotenv import load_dotenv
+from quadhoral_extract import get_connection
+
+
+def load_data(data: list[list]) -> None:
+    '''Inserts data into a postgres database'''
+    ...
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+    load_dotenv()
+    load_data([[]])
