681bebca8ea867074c72d66bc249589fac49e9a6
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-10 10:04:20 +0100

    Implemented load function to pass tests

--- weekly-openmeteo/load.py
+++ weekly-openmeteo/load.py
@@ -5,7 +5,13 @@ from extract import get_connection
 
 def load_data(data: list[list]) -> None:
     '''Inserts data into a postgres database'''
-    ...
+    query = '''INSERT INTO solar_feature (county_id,sunrise_timestamp,sunset_timestamp)
+    VALUES (%s,%s,%s)'''
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.executemany(query, data)
+            conn.commit()
+
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
