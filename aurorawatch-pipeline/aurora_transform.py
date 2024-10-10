0df9a97fc2120a1af8bfd2eec25a24cb1fd6d678
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-10 14:41:01 +0100

    Added to transform functions

--- aurorawatch-pipeline/aurora_transform.py
+++ aurorawatch-pipeline/aurora_transform.py
@@ -1,2 +1,27 @@
 '''Script to transform the extracted data from the AuroraWatch UK API into a format
 ready to be loaded.'''
+
+import logging
+from datetime import datetime as dt
+
+from aurora_extract import extract
+
+ALERT_COLOURS = {'Green': 1, 'Yellow': 2, 'Amber': 3, 'Red': 4}
+
+
+def get_colour_id(colour: str) -> int:
+    ''''''
+
+
+def get_current_datetime() -> str:
+    ''''''
+
+
+def transform(colour: str) -> tuple:
+    ''''''
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+    extracted_data = extract()
