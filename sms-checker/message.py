600593e38da196e021fa08cb1e2ce1494a8cb7d9
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-10 12:20:06 +0100

    Formatted code

--- sms-checker/message.py
+++ sms-checker/message.py
@@ -7,6 +7,7 @@ from boto3 import client
 def send_sms(subscribers: list[dict]) -> None:
     '''Uses boto3 to send messages to subscribers by sms'''
     sms = get_client('sns')
+    logging.info('Client established for SNS')
     for sub in subscribers:
         if sub['body']:
             message = 'STARWATCH ALERT'
@@ -14,34 +15,45 @@ def send_sms(subscribers: list[dict]) -> None:
             if not sub['at']:
                 message += f"{sub['body'].capitalize()} will be visible tonight in your county!"
             else:
-                message += f"\n{sub['body'].capitalize()} will be visible tonight at {sub['at'].strftime('%H:%M')} in your county!"
+                message += f"\n{sub['body'].capitalize()} will be visible tonight"
+                message += f"at {sub['at'].strftime('%H:%M')} in your county!"
             message += "\n Go to our dashboard for more information."
-
-            sms.publish(PhoneNumber=sub['phone'],
+            logging.info('Message constructed successfully')
+            response = sms.publish(PhoneNumber=sub['phone'],
                         Message=message)
+            logging.info('Message sent for subscriber: %s',sub['user'])
+            logging.info('SNS response received: %s', response)
+        else:
+            logging.warning('Invalid data in: %s', sub)
 
 
 def send_email(subscribers: list[dict]) -> None:
     '''Uses boto3 to send messages to subscribers by ses'''
     ses = get_client('ses')
+    logging.info('Client established for SES')
     for sub in subscribers:
         if sub['body']:
             message = 'STARWATCH ALERT'
             message += f'\n\n Hello {sub['user'].capitalize()},\n\n'
-            
+
             if not sub['at']:
                 subject = f"ALERT: {sub['body'].capitalize()} visible Today"
                 message += f"{sub['body'].capitalize()} will be visible tonight in your county!"
             else:
-                subject = f"ALERT: {sub['body'].capitalize()} visible at {sub['at'].strftime('%H:%M')} Today"
-                message += f"{sub['body'].capitalize()} will be visible tonight at {
-                    sub['at'].strftime('%H:%M')} in your county!"
+                subject = f"ALERT: {sub['body'].capitalize()} visible at"
+                subject += f"{sub['at'].strftime('%H:%M')} Today"
+                message += f"{sub['body'].capitalize()} will be visible tonight at"
+                message += f"{sub['at'].strftime('%H:%M')} in your county!"
             message += "\n Go to our dashboard for more information."
-
-            ses.send_email(Source=ENV['FROM'],
+            logging.info('Message constructed successfully')
+            response = ses.send_email(Source=ENV['FROM'],
                            Destination={'ToAddresses': [sub['email']]},
                            Message={'Subject': {'Data': subject},
                                     'Body': {'Text': {'Data': message}}})
+            logging.info('Message sent for subscriber: %s', sub['user'])
+            logging.info('SES response received: %s', response)
+        else:
+            logging.warning('Invalid data in: %s',sub)
 
 
 def get_client(service: str) -> client:
