a62e8e44c365e4875bd37aaedd9c1b26a78d751e
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-10 16:57:44 +0100

    Finished tests for transform

--- aurorawatch-pipeline/test_aurora_transform.py
+++ aurorawatch-pipeline/test_aurora_transform.py
@@ -1,8 +1,9 @@
 '''Test file for the AuroraWatch extract script.'''
 # pylint: disable=W0212
 
-
 from unittest.mock import patch
+from datetime import datetime as dt
+
 import pytest
 
 from aurora_transform import get_colour_id, get_current_datetime, transform
@@ -42,6 +43,41 @@ class TestGetColourID():
 class TestGetCurrentDatetime():
     '''Tests for the get current datetime function.'''
 
+    def test_get_current_datetime_success(self):
+        '''Tests that a string is returned from get current datetime.'''
+        result = get_current_datetime()
+
+        assert isinstance(result, str)
+
+    def test_get_current_datetime_correct_format(self):
+        '''Tests that the sting returned is in the correct format by turning it back
+        to a datetime object.'''
+        result = get_current_datetime()
+
+        converted_result = dt.strptime(result, "%Y-%m-%d %H:%M:%S")
+
+        assert isinstance(converted_result, dt)
+
 
 class TestTransform():
     '''Tests for the transform function.'''
+
+    @patch('aurora_transform.get_colour_id')
+    @patch('aurora_transform.get_current_datetime')
+    def test_transform_calls_success(self, mock_get_current_datetime, mock_get_colour_id):
+        '''Tests that transform returns a correct tuple.'''
+        mock_get_colour_id.return_value = 1
+        mock_get_current_datetime.return_value = '2024-10-07 08:15:30'
+
+        result = transform('')
+
+        assert result == ('2024-10-07 08:15:30', 1)
+
+    @patch('aurora_transform.get_colour_id')
+    @patch('aurora_transform.get_current_datetime')
+    def test_transform_calls_functions(self, mock_get_current_datetime, mock_get_colour_id):
+        '''Tests that the correct functions are called inside the transform.'''
+        transform('')
+
+        assert mock_get_current_datetime.called
+        assert mock_get_colour_id.called
