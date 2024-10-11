3730c7889483211aead95b703fcd4ffaeb514914
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-11 09:40:26 +0100

    Made env variabls consistent in naming

--- weekly-openmeteo/load.py
+++ weekly-openmeteo/load.py
@@ -1,15 +1,16 @@
 '''This script loads the formatted sunrise and sunset times onto the postgres database'''
 import logging
+from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 from extract import get_connection
 
 def load_data(data: list[list]) -> None:
     '''Inserts data into a postgres database'''
     query = '''INSERT INTO solar_feature (county_id,sunrise_timestamp,sunset_timestamp)
-    VALUES (%s,%s,%s)'''
+    VALUES %s'''
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.executemany(query, data)
+            execute_values(cur,query, data)
             conn.commit()
 
 
