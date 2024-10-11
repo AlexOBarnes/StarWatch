803632502d4c2e2493966b424b82694f2de7addb
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-11 09:53:32 +0100

    Rolled back changes

--- weekly-openmeteo/load.py
+++ weekly-openmeteo/load.py
@@ -1,16 +1,15 @@
 '''This script loads the formatted sunrise and sunset times onto the postgres database'''
 import logging
-from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 from extract import get_connection
 
 def load_data(data: list[list]) -> None:
     '''Inserts data into a postgres database'''
     query = '''INSERT INTO solar_feature (county_id,sunrise_timestamp,sunset_timestamp)
-    VALUES %s'''
+    VALUES (%s,%s,%s)'''
     with get_connection() as conn:
         with conn.cursor() as cur:
-            execute_values(cur,query, data)
+            cur.executemany(query, data)
             conn.commit()
 
 
