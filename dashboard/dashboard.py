3cf782c75bb3c928c3e04cd63cfe771c66a2ac62
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-16 12:47:53 +0100

    Restructured dashboard according to wireframe

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -12,7 +12,8 @@ from email_validator import validate_email as ve, EmailNotValidError
 import phonenumbers
 from phonenumbers import NumberParseException
 from load_dashboard_data import connect_to_db, load_from_starwatch_rds, load_forecasts_by_county_name, load_celestial_body_information
-from nasa_pipeline import nasa_pipeline, get_image_of_the_day, get_iss_location
+from nasa_pipeline import nasa_pipeline, get_image_of_the_day, get_iss_location, get_moon_phase
+from aurora_map import create_aurora_map, create_visibility_map
 
 load_dotenv()
 st. set_page_config(layout="wide")
@@ -62,33 +63,54 @@ page = st.sidebar.selectbox('Navigate', [
 
 # Home page, where the user first interacts with the dashboard by default.
 if page == 'Home':
-   st.title('⭐ Starwatch Data Dashboard ⭐')
-   st.write("""At StarWatch, we are dedicated to inspiring curiosity about the universe.
+    st.title('⭐ Starwatch Data Dashboard ⭐')
+    st.write("""At StarWatch, we are dedicated to inspiring curiosity about the universe.
             Our mission is to provide accessible tools and resources for amateur astronomers and hobbyists
             stargazers so that anyone can enjoy the wonders of space.""")
-   nasa_pipeline()
-   image_title, image = get_image_of_the_day()
-   iss = get_iss_location()
-   col1, col2 = st.columns(2)
+   
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header('Current Aurora Status')
+        aurora = create_aurora_map()
+        if aurora:
+            st.pyplot(aurora)
+    with col2:
+        st.header('Current Visibility')
+        visibility = create_visibility_map()
+        if visibility:
+            st.pyplot(visibility)
 
-   with col1:
-       st.header('Image of the day')
-       st.write(f'{image_title} - {datetime.now().strftime("%d/%m/%Y")}')
-       if 'youtube' in image:
-           st.video(image)
-       else:
-           st.image(image)
+    nasa_pipeline()
+    image_title, image = get_image_of_the_day()
+    iss = get_iss_location()
+    col1, col2 = st.columns(2)
+    moon = get_moon_phase()
+    with col1:
+        st.header('Image of the day')
+        st.write(f'{image_title} - {datetime.now().strftime("%d/%m/%Y")}')
+        if 'youtube' in image:
+            st.video(image)
+        else:
+            st.image(image)
 
-   with col2:
-       st.header('Current ISS Location')
-       st.write(f'Last updated: {
+    with col2:
+        st.header("Today's Moon Phase")
+        st.markdown(
+            f"<div style='text-align: center;'><img src='{
+                moon}' width='300'></div>",
+            unsafe_allow_html=True
+        )
+
+
+    st.header('Current ISS Location')
+    st.write(f'Last updated: {
                 iss["timestamp"].strftime("%H:%M:%S %d/%m/%Y")}')
-       iss_df = pd.DataFrame({
-           'latitude': [float(iss['latitude'])],
-           'longitude': [float(iss['longitude'])]
-       })
-       zoom_level = 1.5
-       st.map(iss_df, zoom=zoom_level)
+    iss_df = pd.DataFrame({
+        'latitude': [float(iss['latitude'])],
+        'longitude': [float(iss['longitude'])]
+    })
+    zoom_level = 1.5
+    st.map(iss_df, zoom=zoom_level)
 
 
 # The part of the dashboard visualising weather and its effect on stargazing.
