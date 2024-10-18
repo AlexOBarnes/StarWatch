24d75afdb25c4805a7c19987d93f0d64b56dcf8f
CalebMathers <caleb-mathers@hotmail.co.uk>
2024-10-18 10:10:43 +0100

    Added imports to tests

--- dashboard/test_aurora_map.py
+++ dashboard/test_aurora_map.py
@@ -3,3 +3,10 @@
 
 from unittest.mock import patch, MagicMock
 import matplotlib.pyplot as plt
+
+from aurora_map import (get_aurora_data, get_cloud_data,
+                        map_cloud_coverage, map_region_colours, create_aurora_map,
+                        get_average_cloud_data, get_body_visible_regions,
+                        map_body_visibility_regions_colours, create_body_visibility_map,
+                        map_average_cloud_coverage, get_average_visibility_data,
+                        map_average_visibility_colour, create_visibility_map)
